1f3d463ef1e607016adfb3fb43ea082f65e5432d
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-03-20 19:15:58 -0700

    changed wm config

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -1,7 +1,6 @@
 ---------------------------
 -- Default awesome theme --
 ---------------------------
-
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
@@ -9,28 +8,67 @@ local dpi = xresources.apply_dpi
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
 
-local theme = {}
+-- @Sainhe gruvbox material theme
+local palette = {
+    black = "#292828",
+    red = "#ea6962",
+    green = "#a9b665",
+    yellow = "#d8a657",
+    blue = "#7daea3",
+    magenta = "#d3869b",
+    cyan = "#89b482",
+    white = "#d4be98",
+    default = "#d4be98"
+}
 
-theme.font          = "Fira Code 8"
-theme.nerd_font     = "Fira Code Nerd Font 8"
+-- based off the original gruvbox theme
+local palette_original = {
+    black = "#282828",
+    red = "#cc241d",
+    green = "#89d185",
+    yellow = "#d79921",
+    blue = "#458588",
+    magenta = "#d3869b",
+    cyan = "#689d6a",
+    white = "#ebdbb2",
+    default = "#ebdbb2"
+}
 
--- Themes are based off of gruvbox material hard dark and soft dark mixed
-theme.bg_normal     = "#1d2021"
-theme.bg_focus      = "#32302f"
-theme.bg_urgent     = "#ea6962"
-theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
+local theme = {
+    font = "Fira Code 8",
+    nerd_font = "Fira Code Nerd Font 8",
 
-theme.fg_normal     = "#d4be98"
-theme.fg_focus      = "#d4be98"
-theme.fg_urgent     = "#1d2021"
-theme.fg_minimize   = "#1d2021"
+    -- Themes are based off of gruvbox material hard dark and soft dark mixed
 
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(3)
-theme.border_normal = "#1d2021"
-theme.border_focus  = "#d4be98"
-theme.border_marked = "#ea6962"
+    -- Taskbar resting color
+    bg_normal = palette.black,
+    -- Taskbar focused color,
+    bg_focus = palette.white,
+    -- Error messages
+    bg_urgent = palette.red,
+    -- ?
+    bg_minimize = palette.cyan,
+    -- ?
+    bg_systray = palette_original.black,
+
+    -- Taskbar text resting color
+    fg_normal = palette.white,
+    -- Taakbar text focus color
+    fg_focus = palette.black,
+    -- Text color of errore messages
+    fg_urgent = palette_original.black,
+    -- ?
+    fg_minimize = palette_original.black,
+
+    -- No gaps between tiles
+    useless_gap = dpi(0),
+    -- No borders, cause they make the theme look unmaterial
+    border_width = dpi(0),
+    border_normal = palette.black,
+    border_focus = palette.yellow,
+    -- ?
+    border_marked = "#ea6962"
+}
 
 -- There are other variable sets
 -- overriding the default one when
@@ -43,16 +81,14 @@ theme.border_marked = "#ea6962"
 -- prompt_[fg|bg|fg_cursor|bg_cursor|font]
 -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
 -- Example:
---theme.taglist_bg_focus = "#ff0000"
+-- theme.taglist_bg_focus = "#ff0000"
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
 theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
-    taglist_square_size, theme.fg_normal
-)
+                                taglist_square_size, theme.fg_normal)
 theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
-    taglist_square_size, theme.fg_normal
-)
+                                  taglist_square_size, theme.fg_normal)
 
 -- Variables set for theming notifications:
 -- notification_font
@@ -63,14 +99,14 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 theme.menu_height = dpi(0)
-theme.menu_width  = dpi(0)
+theme.menu_width = dpi(0)
 
 -- You can add as many variables as
 -- you wish and access them by using
 -- beautiful.variable in your rc.lua
---theme.bg_widget = "#cc0000"
+-- theme.bg_widget = "#cc0000"
 
 -- Define the image to load
 --[[ theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
@@ -102,22 +138,22 @@ theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/m
 theme.wallpaper = "/home/spicy/.config/awesome/persona-city.jpg"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
+theme.layout_fairv = themes_path .. "default/layouts/fairvw.png"
+theme.layout_floating = themes_path .. "default/layouts/floatingw.png"
+theme.layout_magnifier = themes_path .. "default/layouts/magnifierw.png"
+theme.layout_max = themes_path .. "default/layouts/maxw.png"
+theme.layout_fullscreen = themes_path .. "default/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path .. "default/layouts/tilebottomw.png"
+theme.layout_tileleft = themes_path .. "default/layouts/tileleftw.png"
+theme.layout_tile = themes_path .. "default/layouts/tilew.png"
+theme.layout_tiletop = themes_path .. "default/layouts/tiletopw.png"
+theme.layout_spiral = themes_path .. "default/layouts/spiralw.png"
+theme.layout_dwindle = themes_path .. "default/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path .. "default/layouts/cornernww.png"
+theme.layout_cornerne = themes_path .. "default/layouts/cornernew.png"
+theme.layout_cornersw = themes_path .. "default/layouts/cornersww.png"
+theme.layout_cornerse = themes_path .. "default/layouts/cornersew.png"
 
 -- Generate Awesome icon:
 --[[ theme.awesome_icon = theme_assets.awesome_icon(
