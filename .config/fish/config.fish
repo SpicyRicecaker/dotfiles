03f87f8e6b9184ab81b97355471e2bf13d883ce1
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-04-16 23:38:42 -0700

    cannot for the life of me figure out how to do conditionals in nushell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,13 +34,6 @@ set -x QT_XCB_TABLET_LEGACY_COORDINATES ""
 # Path
 set PATH $HOME/.opt/texlab-x86_64-linux $DENO_INSTALL/bin $HOME/.opt/dart-sass $HOME/.npm/bin $HOME/.cargo/bin $PATH
 
-# Function to swap files
-function swap
-  mv $argv[2] tmp_file_for_swap
-  mv $argv[1] $argv[2]
-  mv tmp_file_for_swap $argv[1]
-end
-
 # On login, open the xorg server!
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
--- .config/nvim/lua/_statusline.lua
+++ .config/nvim/lua/_statusline.lua
@@ -1,27 +1,25 @@
 -- # Status line
-
 -- ## Status line config
-
 local lualine = require('lualine')
 lualine.theme = 'gruvbox_material'
 lualine.separator = '|'
 lualine.sections = {
-  lualine_a = { 'mode' },
-  lualine_b = { 'branch' },
-  lualine_c = { 'filename', LspStatus },
-  lualine_x = { 'encoding', 'fileformat', 'filetype' },
-  lualine_y = { 'progress' },
-  lualine_z = { 'location'  },
-  lualine_diagnostics = {  }
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {'filename', LspStatus},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'},
+    lualine_diagnostics = {}
 }
 
 lualine.inactive_sections = {
-  lualine_a = {  },
-  lualine_b = {  },
-  lualine_c = { 'filename' },
-  lualine_x = { 'location' },
-  lualine_y = {  },
-  lualine_z = {   }
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
 }
-lualine.extensions = { 'fzf' }
+lualine.extensions = {'fzf'}
 -- lualine.status()
