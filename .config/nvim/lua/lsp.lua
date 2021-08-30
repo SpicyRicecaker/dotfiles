6a74eeb1e7ca6b7f06eb605d9c5a871327ac3563
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-08-30 05:31:28 -0700

    tmux vi

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -32,7 +32,7 @@ local function load ()
 	-- NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
 	-- other plugin before putting this into your config.
 	-- inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : <SID>check_back_space() ? "\<TAB>" : coc#refresh()
-	m('i', '<Tab>', 'v:lua.smart_tab()', {expr = true, noremap = true})
+	-- m('i', '<Tab>', 'v:lua.smart_tab()', {expr = true, noremap = true})
 	-- inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
 	m('i', '<S-Tab>', 'v:lua.smart_shift_tab()', {expr = true, noremap = true})
 
