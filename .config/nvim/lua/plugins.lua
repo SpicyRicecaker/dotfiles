bfbf584594cb2ca9e35f80af9a5af74a4f03435e
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-09-03 05:42:42 -0700

    refactored coc.nvim cause we're trying out rust-tools.nvim lol

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,25 +3,44 @@ return require'packer'.startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    -- Post-install/update hook with neovim command
+    -- Highlight
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
+    -- Fuzzy search
     use {'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim'}
 
+    -- Colorscheme
     use 'sainnhe/gruvbox-material'
 
+    -- LSP!
     use 'neovim/nvim-lspconfig'
+    -- Autosave makes lsp feel a lot better!
+    use 'Pocco81/AutoSave.nvim'
+    -- Testing out native lsp rn, thanks to autosave
+    -- use {'neoclide/coc.nvim', branch = 'release'}
 
+    -- Rust!
+    use {
+      'simrat39/rust-tools.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim',
+        'nvim-lua/popup.nvim',
+        'neovim/nvim-lspconfig',
+        'nvim-telescope/telescope.nvim',
+        'mfussenegger/nvim-dap'
+      }
+    }
+
+    -- Comments!
     use 'b3nj5m1n/kommentary'
 
-    use {'neoclide/coc.nvim', branch = 'release'}
-
-    use 'Pocco81/AutoSave.nvim'
-
+    -- Autopairing of open close parens
     use 'windwp/nvim-autopairs'
 
+    -- VSCode sidebar
     use {'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons'}
-    -- Use specific branch, dependency and run lua file after load
+
+    -- Statusline
     use {
         'glepnir/galaxyline.nvim',
         branch = 'main',
