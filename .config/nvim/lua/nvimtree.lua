f3928770c274a2633c004ff5fce5775bbf3870de
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2022-06-11 22:55:02 -0700

    removed stuff

--- .config/nvim/lua/nvimtree.lua
+++
@@ -1,3 +0,0 @@
-(function ()
-	vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle <CR>", {noremap = true, silent = true})
-end)()
--- .config/wezterm/wezterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-  default_cursor_style = "SteadyUnderline",
-  colors = {
-    foreground = "white",
-    background = "black",
-    cursor_border = "white",
-  },
-  harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
-}
