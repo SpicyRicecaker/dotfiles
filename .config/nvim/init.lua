021bf76ab928a454d063bdaae17f927d94c6f27b
SpicyRicecaker <awesomenessisgolden@gmail.com>
2022-06-27 17:56:09 -0700

    Small changes to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,10 @@ vim.g.mapleader = " "
 
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
+vim.o.ignorecase = true
+-- never use mouse in vim lol
+-- also it's distracting when you're typing but misclick trackpad
+vim.o.mouse = nil
 -- line number relative
 -- vim.o.relativenumber = true
 -- save on focus shift
@@ -12,7 +16,7 @@ return require('packer').startup(function()
 
     use {
         'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} },
+        requires = { {'nvim-lua/plenary.nvim'}, {'kyazdani42/nvim-web-devicons'} },
         config = function()
             vim.cmd[[
             nnoremap <leader>ff <cmd>Telescope find_files<cr>
@@ -37,10 +41,11 @@ return require('packer').startup(function()
         run = ':TSUpdate',
         config = function()
             require'nvim-treesitter.configs'.setup {
-                ensure_installed = { "lua", "rust", "toml", "markdown"},
+                ensure_installed = { "lua", "rust", "toml", "markdown", "tsx", "typescript", "javascript", "html", "css", "json", "scheme"},
                 highlight = {
                     enable = true,
-                    additional_vim_regex_highlighting = false,
+                    -- I want markdown italics, so enabling this for now
+                    additional_vim_regex_highlighting = true
                 },
                 incremental_selection = {
                     enable = true,
@@ -52,7 +57,9 @@ return require('packer').startup(function()
                     },
                 },
                 indent = {
-                    enable = true
+                    -- currently disabling as it's buggy with rust if let statements
+                    -- vim default indent is good enough
+                    enable = false
                 }
             }
         end,
@@ -73,4 +80,9 @@ return require('packer').startup(function()
             vim.cmd[[colorscheme gruvbox-material]]
         end,
     }
+
+    use 'tpope/vim-surround'
+
+    -- debug 
+    use 'nvim-treesitter/playground'
 end)
